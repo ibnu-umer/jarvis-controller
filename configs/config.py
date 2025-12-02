60a76b60ba4c2261383b0d3751d93c2f46bd4540
= <muhammadriyask11@gmail.com>
2025-12-02 17:09:34 +0530

    feat: add centralized config module for hosts, prots, and general settings

---
+++ configs/config.py
@@ -0,0 +1,9 @@
+WIN_HOST = "0.0.0.0"
+WIN_PORT = 6001
+
+WSL_HOST = "172.29.128.1"     # dynamically read or configured
+WSL_PORT = 6000
+
+PING_INTERVAL = 6  # seconds
+WIN_BASE_URL = f"http://{WIN_HOST}:{WIN_PORT}"
+WSL_BASE_URL = f"http://{WSL_HOST}:{WSL_PORT}"
