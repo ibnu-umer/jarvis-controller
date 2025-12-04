2fd5b1a839c87af999ab08a2677832a83f50dbb4
= <muhammadriyask11@gmail.com>
2025-12-04 14:26:32 +0530

    refactor(core): move FileRegistry to core.registry for centralized managment

--- src/core/registry.py
+++ src/core/registry.py
@@ -1,8 +1,10 @@
-import importlib
+import importlib, json
 import pkgutil
 from modules import __path__ as modules_path
 from core.logger import logger
 from core.base_module import BaseModule
+from configs.config import FILE_REGISTRY_PATH
+from pathlib import Path
 
 
 
@@ -66,3 +68,63 @@ def load_registry():
     MODULE_REGISTRY.load_all_modules()   
     logger.info(f"Modules loaded: {list(MODULE_REGISTRY._instances.keys())}")     
 
+
+
+
+class FileRegistry:
+    def __init__(self, registry_file: str = FILE_REGISTRY_PATH):
+        self.registry_file = Path(registry_file)
+        self.registry_file.parent.mkdir(parents=True, exist_ok=True)
+
+        if not self.registry_file.exists():
+            self.registry_file.write_text(json.dumps({}, indent=4), encoding="utf-8")
+
+        self.entries = {} 
+        self._load_registry()
+
+
+    def _load_registry(self):
+        if self.registry_file.exists():
+            try:
+                with open(self.registry_file, "r", encoding="utf-8") as f:
+                    self.entries = json.load(f)
+                    logger.info("Registry loaded successfully.")
+            except Exception as e:
+                logger.error(f"Error loading registry: {e}")
+                self.entries = {}
+
+
+    def _save_registry(self):
+        try:
+            with open(self.registry_file, "w", encoding="utf-8") as f:
+                json.dump(self.entries, f, indent=4)
+        except Exception as e:
+            logger.error(f"Error saving registry: {e}")
+
+
+    def add_entry(self, name: str, path: str) -> bool:
+        p = Path(path)
+        if not p.exists():
+            return False
+        self.entries[name] = str(p.resolve())
+        self._save_registry()
+        return True
+    
+
+    def remove_entry(self, name: str) -> bool:
+        if name in self.entries:
+            self.entries.pop(name)
+            self._save_registry()
+            return True
+        return False
+    
+
+    def get_path(self, name: str) -> str | None:
+        return self.entries.get(name)
+    
+
+    def list_entries(self) -> dict:
+        return self.entries.copy()
+    
+
+registry = FileRegistry()
