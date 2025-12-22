c0765f59bf3f26cbfd11890e0ce29e510609af0f
= <muhammadriyask11@gmail.com>
2025-12-22 14:20:03 +0530

    fix(config): update WSL connect host for Windows client

--- configs/config.py
+++ configs/config.py
@@ -11,9 +11,9 @@ WSL_PORT = 6000
 
 PING_INTERVAL = 6  # seconds
 WIN_BASE_URL = f"http://{WIN_HOST}:{WIN_PORT}"
-WSL_BASE_URL = f"http://{WSL_HOST}:{WSL_PORT}"
 
-
+WSL_CONNECT_HOST = "172.29.130.227"
+WSL_BASE_URL = f"http://{WSL_CONNECT_HOST}:6000"
 
 def get_resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
