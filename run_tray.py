5be852ae4e76d263adaa4dd2fbe04749a9191703
= <muhammadriyask11@gmail.com>
2025-12-22 14:21:40 +0530

    fix(run_tray): add exception handling to avoid silent failing

--- run_tray.py
+++ run_tray.py
@@ -7,9 +7,12 @@ SRC_PATH = os.path.join(os.path.dirname(__file__), "src")
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
 
-from tray.tray_app import JarvisTray
-from api.listener import WindowsListener 
-from core.registry import load_registry
+try:
+    from tray.tray_app import JarvisTray
+    from api.listener import WindowsListener 
+    from core.registry import load_registry
+except Exception as e:
+    print(e)
 
 
 
@@ -24,11 +27,14 @@ def run_tray():
 
 
 if __name__ == "__main__":
-    load_registry()
+    try:
+        load_registry()
 
-    windows_listener = WindowsListener()
-    listener_thread = threading.Thread(target=windows_listener.start, daemon=True)
-    listener_thread.start()
+        windows_listener = WindowsListener()
+        listener_thread = threading.Thread(target=windows_listener.start, daemon=True)
+        listener_thread.start()
 
 
-    run_tray()
+        run_tray()
+    except Exception as e:
+        print(e)
