a5998a0df98983cf828010c681a6a2bf309cccc0
= <muhammadriyask11@gmail.com>
2026-01-10 17:47:58 +0530

    feat(screen_time_module): remove vlc player from timeout

--- src/modules/screentime_module.py
+++ src/modules/screentime_module.py
@@ -138,8 +138,8 @@ class ScreenTimeModule(BaseModule):
             try:
                 idle = self._get_idle_seconds()
                 now = time.time()
-
-                if idle >= self._idle_timeout_seconds:
+   
+                if idle >= self._idle_timeout_seconds and self._current_app not in ("vlc.exe"):
                     if self._current_app and self._current_start:
                         elapsed = now - self._current_start
                         self._add_seconds(self._current_app, elapsed)
